726021cfd750274cf124fc46c174040b60306534
iagosm <iagosousa201486@gmail.com>
2024-12-18 23:35:04 -0300

    Abstração, event loop, emiitter, sync and async

---
+++ 2_FUNDAMENTOS/13 _sync_async/assincrono.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+console.log("Inicio")
+
+fs.writeFile("arquivo.txt", "Olaaa", function(err) {
+  setTimeout(function () {
+    console.log('criado')
+  }, 1000)
+});
+
+console.log("Fim")
